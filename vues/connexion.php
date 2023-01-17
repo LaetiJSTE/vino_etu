e240f3395c860a11add86ebd428faae45c42119f
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-17 16:35:35 -0500

    ajout formulaire

---
+++ vues/connexion.php
@@ -0,0 +1,13 @@
+<section class="form">
+
+    <form  method="post" action="" >
+        <h5 class="form-title">Connexion</h5>   
+        <input type="email" name="" id="" placeholder="courriel" required>
+        <input type="password" placeholder="Mot de passe" name="" id="" required>
+        <button type="submit" value="Connexion">Se connecter</button><br>
+
+        <a href="">Créer un compte</a>
+    </form>
+
+
+</section>
