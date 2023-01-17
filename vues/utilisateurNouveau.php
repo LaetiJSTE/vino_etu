dd36cdfed846c19dab6eedb1786974a244eec4e6
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-17 16:47:21 -0500

    creation vues pour nouvel utilisateur et ajout formulaire

---
+++ vues/utilisateurNouveau.php
@@ -0,0 +1,14 @@
+<section class="form">
+
+    <form method="post" action="">
+        <h5 class="form-title">créer un compte</h5>
+        <input type="text" placeholder="Nom" name="uti_nom" id="uti_nom" required>
+        <input type="text" placeholder="Courriel" name="uti_courriel" id="uti_courriel" required>
+        <input type="password" placeholder="Mot de passe" name="uti_mdp" id="uti_mdp" required>
+
+
+        <button type="submit">s'inscrire</button> <br> <br>
+        <a href="?requete=connexion">Se connecter</a>
+    </form>
+
+</section>
