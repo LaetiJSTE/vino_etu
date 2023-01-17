a69eeaa0fa74b1a1ffb30f985382f3bc2938533e
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2023-01-17 16:53:32 -0500

    retrait vues pied.php dans l'accueil

--- Controler.class.php
+++ Controler.class.php
@@ -61,7 +61,7 @@ class Controler
             $data = $bte->getListeBouteilleCellier();
 			include("vues/entete.php");
 			include("vues/cellier.php");
-			include("vues/pied.php");
+			
                   
 		}
 
